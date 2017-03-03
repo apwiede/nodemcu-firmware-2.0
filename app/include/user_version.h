376596d38aee2838538f99e4960a7b5f2c004ab5
apwiede <arnulf@wiedemann-pri.de>
2017-03-03 20:28:27 +0100

    fixes.

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,9 +6,9 @@
 #define NODE_VERSION_REVISION	0U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 2.0.0"
+#define NODE_VERSION "NodeMCU custom build by apwiede\n\tbranch: dev\n\tcommit: f1f8eb3371edcf60dd466e7a822f211cc8c1a72b\n\tSSL: true\n\tmodules: compmsg,file,net,rboot,tmr,uart,wifi\n"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  "unspecified"
+#define BUILD_DATE "\tbuilt on: 2017-03-03 19:50\n"
 #endif
 
 extern char SDK_VERSION[];
